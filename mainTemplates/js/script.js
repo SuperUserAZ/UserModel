c2b48b3b889a84c65243be64aa4690002a74f776
super0user <88291146+super0user@users.noreply.github.com>
2022-02-05 00:54:25 +0400

    Update script.js

--- mainTemplates/js/script.js
+++ mainTemplates/js/script.js
@@ -1,13 +1,32 @@
 const MainLogBtn = document.querySelector('#main-log-btn');
 const MainSignBtn = document.querySelector('#main-sign-btn');
+const LogBtn = document.querySelector('#log-btn');
+const SignBtn = document.querySelector('#sign-btn');
+
+function ShowMainPage() {
+    document.location.assign('');
+}
 
 function ShowLoginPage() {
     document.location.assign('login');
 }
-
 function ShowSignUpPage() {
     document.location.assign('signup');
 }
 
-MainLogBtn.onclick = ShowLoginPage;
-MainSignBtn.onclick = ShowSignUpPage;
+function ShowProfilePage() {
+    document.location.assign('profile');
+}
+if (MainLogBtn) {
+    MainLogBtn.onclick = ShowLoginPage;
+}
+
+if (MainSignBtn) {
+    MainSignBtn.onclick = ShowSignUpPage;
+}
+else if (LogBtn) {
+    LogBtn.onclick = ShowMainPage;
+}
+else if (SignBtn) {
+    LogBtn.onclick = ShowProfilePage;
+}
